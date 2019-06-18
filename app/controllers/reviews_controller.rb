f76a49d3f1eb6525df5906ffa9e2d049f6991a27
hlbtyne <harriet.ballantyne@gmail.com>
2019-06-18 18:19:13 +0100

    fetch and render apps, review creation and deletion

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,12 +5,21 @@ class ReviewsController < ApplicationController
         render json: reviews
     end 
 
-    def destroy 
-
-    end 
-
     def create 
+        newReview = Review.new(comment: params[:comment], app_id: params[:app_id], user_id: params[:user_id])
+        
+        if newReview.save
+            render json: newReview
+        else
+            render json: {errors: "You need to create a valid review, bruv."}, status: 400
+        end
     end 
     
+    def destroy 
+        review = Review.find_by(id: params[:id])
+        review.destroy
+        render json: review
+    end 
+
 
 end
