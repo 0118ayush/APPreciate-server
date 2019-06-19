589cdee960b2f34daccdca5bfb99980fca9e5f18
0118ayush <ayush_gehlot@hotmail.com>
2019-06-19 15:23:38 +0100

    signin and out backend partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,13 @@ class UsersController < ApplicationController
         end 
     end 
 
+    def signin
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: user 
+        else 
+            render json: {error: "Invalid username or password combination"}, status: 401
+        end 
+    end 
+
 end
